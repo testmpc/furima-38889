6f05473a58a8142c3e456748cf2a31018f08b4fd
testmpc <m.takuma950220+github@gmail.com>
2023-06-02 06:32:10 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_furigana, presence: true
-  validates :first_furigana, presence: true
+  validates :last_name, :first_name, presence: true
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は、全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :last_furigana, :first_furigana, presence: true
+  validates :last_furigana, :first_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は、全角のカタカナで入力してください' }
   validates :date_of_birth, presence: true
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
