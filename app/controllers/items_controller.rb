35634b6a8016247c4379c99a745c2c8e6b560a73
testmpc <m.takuma950220+github@gmail.com>
2023-06-03 21:10:45 +0900

    コントローラ、モデル、ビュー大枠作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
-  # def index
-  # end
+  before_action :authenticate_user!, only: [:new]
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, locals: { item: @item }
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :situation_id, :delivery_charge_id, :region_of_origin_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  # has_one    :order, dependent: :destroy
+
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :situation_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :region_of_origin_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
