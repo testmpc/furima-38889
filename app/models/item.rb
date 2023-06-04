a09572df31bd305c2e7397d511b8ef568553f793
testmpc <m.takuma950220+github@gmail.com>
2023-06-04 14:58:28 +0900

    itemバリデーション完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   # has_one    :order, dependent: :destroy
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :situation
   belongs_to :shippingfee
   belongs_to :prefecture
   belongs_to :deliveryskd
+  
   has_one_attached :image
 
   validates :name, presence: true
@@ -18,4 +21,7 @@ class Item < ApplicationRecord
   validates :shippingfee_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :deliveryskd_id, numericality: { other_than: 1, message: "can't be blank" }
+  
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999},
+  format: { with: /\A[0-9]+\z/, message: "Please enter in half-width digit number"}
 end
