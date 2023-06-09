19135ef51f861813c6fd61ee7a6562f1cb118a79
testmpc <m.takuma950220+github@gmail.com>
2023-06-09 20:59:13 +0900

    rubocop完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    unless @item.user_id == current_user.id
-      redirect_to action: :index
-    end
+    return if @item.user_id == current_user.id
+
+    redirect_to action: :index
   end
 
   def update
