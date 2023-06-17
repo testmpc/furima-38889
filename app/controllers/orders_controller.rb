9c4de5b3ef7cf05bdeb9dae71b9b77d93d9b8f65
testmpc <m.takuma950220+github@gmail.com>
2023-06-17 18:39:50 +0900

    クレジットカード機能追加完了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,39 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
 
   def index
-  end
-
-  def new
-    @order_address = DonationAddress.new
+    @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id || Order.where(item_id: @item.id).exists?
+      redirect_to root_path
+    end
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order_address = DonationAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
-      render :new
+      render :index, locals: {item: @item}
     end
   end
 
   private
 
   def order_params
-    params.require(:order_address).permit(post_code: post_code, prefecture_id: prefecture_id,municipality: municipality,
-                                          block_number: block_number,building: building,
-                                          tel_number: tel_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :block_number,
+                    :building, :tel_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: Item.find(params[:item_id]).price,  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+    end
 end
